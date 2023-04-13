5b88be9a587f8aedac6cf3be217e75ee7a642280
timmb <772105+timmb@users.noreply.github.com>
2023-04-13 16:10:17 -0400

    Split IP stuff from LoadingScreen as also used in Main scene. Efforts to fix button interaction on VR - update XR Interaction Toolkit

--- Assets/Listening Effort/Scripts/LoadingScreen.cs
+++ Assets/Listening Effort/Scripts/LoadingScreen.cs
@@ -1,8 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Sockets;
+
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -17,17 +16,8 @@ public class LoadingScreen : MonoBehaviour
 
     public void Awake()
     {
-        var host = Dns.GetHostEntry(Dns.GetHostName());
-        string ipAddresses = Dns.GetHostEntry(Dns.GetHostName())
-            .AddressList
-            .Where(ip => ip.AddressFamily == AddressFamily.InterNetwork)
-            .Where(ip => !(ip.ToString() == "127.0.0.1"))
-            .Select(ip => ip.ToString())
-            .Select(ipAddresses => $"{ipAddresses}:{OSCController.listenPort}")
-            .Aggregate((head, tail) => $"{head}\n{tail}");
-
+        
         loadingText.text = loadingText.text
-            .Replace("{IP_ADDRESS}", $"{ipAddresses}")
             .Replace("{VIDEO_DIRECTORIES}", videoChecker.videoDirectories.Aggregate((a,b) => $"{a}\n{b}"));
 
         videoChecker.videosAreOKChanged += (sender, videosAreOK) => updateButtons();
