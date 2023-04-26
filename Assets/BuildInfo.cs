20aa0e877391505eeb6b036b5e91af4b2f49f629
timmb <772105+timmb@users.noreply.github.com>
2023-04-26 14:00:29 -0400

    Further git fixes to BuildInfo file

--- Assets/BuildInfo.cs
+++ Assets/BuildInfo.cs
@@ -1,1 +1,3 @@
+// Changes to this file should not be tracked into the repo
 public static class BuildInfo{public static string BUILD_TIME = "BUILD_TIME_WILL_GO_HERE";}
+// To ignore changes to this file: git update-index --assume-unchanged BuildInfo.cs
